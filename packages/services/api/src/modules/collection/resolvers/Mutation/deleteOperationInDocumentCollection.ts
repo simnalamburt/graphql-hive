d0ee2483b55aff2ba624d79020b5508038ae04c8
Eddy Nguyen <github@eddeee888.me>
2024-07-07 16:21:41 +1000

    Migrate Mutation.deleteOperationInDocumentCollection

---
+++ packages/services/api/src/modules/collection/resolvers/Mutation/deleteOperationInDocumentCollection.ts
@@ -0,0 +1,33 @@
+import { CollectionProvider } from '../../providers/collection.provider';
+import { validateTargetAccess } from '../../validation';
+import { TargetAccessScope, type MutationResolvers } from './../../../../__generated__/types.next';
+
+export const deleteOperationInDocumentCollection: NonNullable<
+  MutationResolvers['deleteOperationInDocumentCollection']
+> = async (_, { selector, id }, { injector }) => {
+  const target = await validateTargetAccess(injector, selector, TargetAccessScope.REGISTRY_WRITE);
+  const operation = await injector.get(CollectionProvider).getOperation(id);
+
+  if (!operation) {
+    return {
+      error: {
+        __typename: 'ModifyDocumentCollectionError',
+        message: 'Failed to locate a operation',
+      },
+    };
+  }
+
+  const collection = await injector
+    .get(CollectionProvider)
+    .getCollection(operation.documentCollectionId);
+  await injector.get(CollectionProvider).deleteOperation(id);
+
+  return {
+    ok: {
+      __typename: 'DeleteDocumentCollectionOperationOkPayload',
+      deletedId: id,
+      updatedTarget: target,
+      updatedCollection: collection!,
+    },
+  };
+};
