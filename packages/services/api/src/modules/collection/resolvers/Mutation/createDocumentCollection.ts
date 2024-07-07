9ce1b9945960742cf2f3711cce955c51c53a6b91
Eddy Nguyen <github@eddeee888.me>
2024-07-07 16:05:44 +1000

    Migrate Mutation.createDocumentCollection

---
+++ packages/services/api/src/modules/collection/resolvers/Mutation/createDocumentCollection.ts
@@ -0,0 +1,18 @@
+import { CollectionProvider } from '../../providers/collection.provider';
+import { validateTargetAccess } from '../../validateTargetAccess';
+import { TargetAccessScope, type MutationResolvers } from './../../../../__generated__/types.next';
+
+export const createDocumentCollection: NonNullable<
+  MutationResolvers['createDocumentCollection']
+> = async (_, { selector, input }, { injector }) => {
+  const target = await validateTargetAccess(injector, selector, TargetAccessScope.REGISTRY_WRITE);
+  const result = await injector.get(CollectionProvider).createCollection(target.id, input);
+
+  return {
+    ok: {
+      __typename: 'ModifyDocumentCollectionOkPayload',
+      collection: result,
+      updatedTarget: target,
+    },
+  };
+};
