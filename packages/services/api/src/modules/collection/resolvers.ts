f304075f505d9a9af2ad1a087fcc54c56ae53571
Eddy Nguyen <github@eddeee888.me>
2024-07-07 16:18:07 +1000

    Migrate Mutation.updateOperationInDocumentCollection

--- packages/services/api/src/modules/collection/resolvers.ts
+++ packages/services/api/src/modules/collection/resolvers.ts
@@ -1,55 +1,9 @@
-import * as zod from 'zod';
-import { fromZodError } from 'zod-validation-error';
 import { TargetAccessScope } from '../auth/providers/scopes';
 import { CollectionModule } from './__generated__/types';
 import { CollectionProvider } from './providers/collection.provider';
 
 export const resolvers: CollectionModule.Resolvers = {
   Mutation: {
-    async updateOperationInDocumentCollection(_, { selector, input }, { injector }) {
-      try {
-        OperationValidationInputModel.parse(input);
-        const target = await validateTargetAccess(
-          injector,
-          selector,
-          TargetAccessScope.REGISTRY_WRITE,
-        );
-        const result = await injector.get(CollectionProvider).updateOperation(input);
-
-        if (!result) {
-          return {
-            error: {
-              __typename: 'ModifyDocumentCollectionError',
-              message: 'Failed to locate a document collection',
-            },
-          };
-        }
-
-        const collection = await injector
-          .get(CollectionProvider)
-          .getCollection(result.documentCollectionId);
-
-        return {
-          ok: {
-            __typename: 'ModifyDocumentCollectionOperationOkPayload',
-            operation: result,
-            updatedTarget: target,
-            collection: collection!,
-          },
-        };
-      } catch (e) {
-        if (e instanceof zod.ZodError) {
-          return {
-            error: {
-              __typename: 'ModifyDocumentCollectionError',
-              message: fromZodError(e).message,
-            },
-          };
-        }
-
-        throw e;
-      }
-    },
     async deleteOperationInDocumentCollection(_, { selector, id }, { injector }) {
       const target = await validateTargetAccess(
         injector,
